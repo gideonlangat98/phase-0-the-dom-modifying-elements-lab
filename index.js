b683e8a9aac87174845983d632d1ca0b5adcfa5c
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-17 02:05:34 +0300

    commit tree 147393b3a2805b8a4694e0d8aca2f82da6b7203f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+//remove main id
+const main = document.getElementById("main");
+main.remove("id");
+
+//create header
+const newHeader = document.createElement("h1");
+
+newHeader.textContent = "Gideon is the champion";
+
+newHeader.id = "victory";
+
+
+
+
+
+
